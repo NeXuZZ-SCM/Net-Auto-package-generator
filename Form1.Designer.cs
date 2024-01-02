102b1fafff0bf29a332f3a9571e005b94da088ef
Cristian.Sguazzin <cristian.sguazzin.ext@errepar.com>
2024-01-02 08:30:34 -0300

    Subida inicial de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace AutoZipDeploy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            BtnOpenDirectory = new Button();
+            lblCompleteFolderPath = new Label();
+            BtnZipFolder = new Button();
+            ChkBLOptionsDelete = new CheckedListBox();
+            TxtBoxNameProject = new TextBox();
+            ChkBLOptionsBackup = new CheckedListBox();
+            LblExcludeFiles = new Label();
+            LblZipedType = new Label();
+            groupBox1 = new GroupBox();
+            groupBox2 = new GroupBox();
+            toolTip1 = new ToolTip(components);
+            groupBox3 = new GroupBox();
+            LblVistaPreviaNamePackage = new Label();
+            toolTip2 = new ToolTip(components);
+            CmbboxProjectName = new ComboBox();
+            groupBox4 = new GroupBox();
+            groupBox5 = new GroupBox();
+            LblDestinyFolder = new Label();
+            BtnDestinyFolder = new Button();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
+            groupBox4.SuspendLayout();
+            groupBox5.SuspendLayout();
+            SuspendLayout();
+            // 
+            // BtnOpenDirectory
+            // 
+            BtnOpenDirectory.Location = new Point(12, 60);
+            BtnOpenDirectory.Name = "BtnOpenDirectory";
+            BtnOpenDirectory.Size = new Size(323, 29);
+            BtnOpenDirectory.TabIndex = 0;
+            BtnOpenDirectory.Text = "1- Seleccionar carpeta";
+            BtnOpenDirectory.UseVisualStyleBackColor = true;
+            BtnOpenDirectory.Click += BtnOpenDirectory_Click;
+            // 
+            // lblCompleteFolderPath
+            // 
+            lblCompleteFolderPath.AutoSize = true;
+            lblCompleteFolderPath.Location = new Point(7, 19);
+            lblCompleteFolderPath.MaximumSize = new Size(400, 20);
+            lblCompleteFolderPath.Name = "lblCompleteFolderPath";
+            lblCompleteFolderPath.Size = new Size(128, 15);
+            lblCompleteFolderPath.TabIndex = 2;
+            lblCompleteFolderPath.Text = "Seleccione una carpeta";
+            // 
+            // BtnZipFolder
+            // 
+            BtnZipFolder.Location = new Point(7, 175);
+            BtnZipFolder.Name = "BtnZipFolder";
+            BtnZipFolder.Size = new Size(306, 23);
+            BtnZipFolder.TabIndex = 3;
+            BtnZipFolder.Text = "2- Empaquetar";
+            BtnZipFolder.UseVisualStyleBackColor = true;
+            BtnZipFolder.Click += BtnZipFolder_Click;
+            // 
+            // ChkBLOptionsDelete
+            // 
+            ChkBLOptionsDelete.FormattingEnabled = true;
+            ChkBLOptionsDelete.Location = new Point(6, 39);
+            ChkBLOptionsDelete.Name = "ChkBLOptionsDelete";
+            ChkBLOptionsDelete.Size = new Size(159, 130);
+            ChkBLOptionsDelete.TabIndex = 4;
+            ChkBLOptionsDelete.ItemCheck += ChkBLOptionsDelete_ItemCheck;
+            // 
+            // TxtBoxNameProject
+            // 
+            TxtBoxNameProject.Location = new Point(176, 22);
+            TxtBoxNameProject.MaxLength = 200;
+            TxtBoxNameProject.Name = "TxtBoxNameProject";
+            TxtBoxNameProject.Size = new Size(141, 23);
+            TxtBoxNameProject.TabIndex = 5;
+            TxtBoxNameProject.TextChanged += TxtBoxNameProject_TextChanged;
+            // 
+            // ChkBLOptionsBackup
+            // 
+            ChkBLOptionsBackup.FormattingEnabled = true;
+            ChkBLOptionsBackup.Location = new Point(176, 39);
+            ChkBLOptionsBackup.Name = "ChkBLOptionsBackup";
+            ChkBLOptionsBackup.Size = new Size(137, 76);
+            ChkBLOptionsBackup.TabIndex = 8;
+            ChkBLOptionsBackup.ItemCheck += ChkBLOptionsBackup_ItemCheck;
+            // 
+            // LblExcludeFiles
+            // 
+            LblExcludeFiles.AutoSize = true;
+            LblExcludeFiles.Location = new Point(6, 19);
+            LblExcludeFiles.Name = "LblExcludeFiles";
+            LblExcludeFiles.Size = new Size(155, 15);
+            LblExcludeFiles.TabIndex = 9;
+            LblExcludeFiles.Text = "Archivos a excluir en deploy";
+            // 
+            // LblZipedType
+            // 
+            LblZipedType.AutoSize = true;
+            LblZipedType.Location = new Point(176, 19);
+            LblZipedType.Name = "LblZipedType";
+            LblZipedType.Size = new Size(90, 15);
+            LblZipedType.TabIndex = 10;
+            LblZipedType.Text = "Tipo de zipeado";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(ChkBLOptionsBackup);
+            groupBox1.Controls.Add(LblExcludeFiles);
+            groupBox1.Controls.Add(LblZipedType);
+            groupBox1.Controls.Add(ChkBLOptionsDelete);
+            groupBox1.Controls.Add(BtnZipFolder);
+            groupBox1.Location = new Point(12, 300);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(323, 207);
+            groupBox1.TabIndex = 11;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Opciones";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(lblCompleteFolderPath);
+            groupBox2.Location = new Point(12, 12);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(323, 42);
+            groupBox2.TabIndex = 12;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Ruta del paquete publicado";
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(LblVistaPreviaNamePackage);
+            groupBox3.Location = new Point(12, 179);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(323, 47);
+            groupBox3.TabIndex = 13;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Vista previa del nombre del paquete";
+            // 
+            // LblVistaPreviaNamePackage
+            // 
+            LblVistaPreviaNamePackage.AutoSize = true;
+            LblVistaPreviaNamePackage.Location = new Point(7, 19);
+            LblVistaPreviaNamePackage.Name = "LblVistaPreviaNamePackage";
+            LblVistaPreviaNamePackage.Size = new Size(274, 15);
+            LblVistaPreviaNamePackage.TabIndex = 0;
+            LblVistaPreviaNamePackage.Text = "Deploy/Backup Nombre_Del_Proyecto Fecha_Hora";
+            // 
+            // CmbboxProjectName
+            // 
+            CmbboxProjectName.DropDownStyle = ComboBoxStyle.DropDownList;
+            CmbboxProjectName.FormattingEnabled = true;
+            CmbboxProjectName.Location = new Point(7, 22);
+            CmbboxProjectName.Name = "CmbboxProjectName";
+            CmbboxProjectName.Size = new Size(158, 23);
+            CmbboxProjectName.TabIndex = 15;
+            CmbboxProjectName.SelectedIndexChanged += CmbboxProjectName_SelectedIndexChanged;
+            // 
+            // groupBox4
+            // 
+            groupBox4.Controls.Add(TxtBoxNameProject);
+            groupBox4.Controls.Add(CmbboxProjectName);
+            groupBox4.Location = new Point(12, 232);
+            groupBox4.Name = "groupBox4";
+            groupBox4.Size = new Size(323, 61);
+            groupBox4.TabIndex = 16;
+            groupBox4.TabStop = false;
+            groupBox4.Text = "Nombre del proyecto";
+            // 
+            // groupBox5
+            // 
+            groupBox5.Controls.Add(LblDestinyFolder);
+            groupBox5.Location = new Point(12, 96);
+            groupBox5.Name = "groupBox5";
+            groupBox5.Size = new Size(323, 42);
+            groupBox5.TabIndex = 17;
+            groupBox5.TabStop = false;
+            groupBox5.Text = "Ruta de destino";
+            // 
+            // LblDestinyFolder
+            // 
+            LblDestinyFolder.AutoSize = true;
+            LblDestinyFolder.Location = new Point(7, 19);
+            LblDestinyFolder.Name = "LblDestinyFolder";
+            LblDestinyFolder.Size = new Size(228, 15);
+            LblDestinyFolder.TabIndex = 0;
+            LblDestinyFolder.Text = "Por defecto el path del paquete publicado";
+            // 
+            // BtnDestinyFolder
+            // 
+            BtnDestinyFolder.Location = new Point(12, 144);
+            BtnDestinyFolder.Name = "BtnDestinyFolder";
+            BtnDestinyFolder.Size = new Size(323, 23);
+            BtnDestinyFolder.TabIndex = 18;
+            BtnDestinyFolder.Text = "1.1- Opcional Carpeta de destino";
+            BtnDestinyFolder.UseVisualStyleBackColor = true;
+            BtnDestinyFolder.Click += BtnDestinyFolder_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(351, 518);
+            Controls.Add(BtnDestinyFolder);
+            Controls.Add(groupBox5);
+            Controls.Add(groupBox4);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Controls.Add(BtnOpenDirectory);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "Form1";
+            Text = "Auto deploy package";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
+            groupBox4.ResumeLayout(false);
+            groupBox4.PerformLayout();
+            groupBox5.ResumeLayout(false);
+            groupBox5.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button BtnOpenDirectory;
+        private Label lblCompleteFolderPath;
+        private Button BtnZipFolder;
+        private CheckedListBox ChkBLOptionsDelete;
+        private TextBox TxtBoxNameProject;
+        private CheckedListBox ChkBLOptionsBackup;
+        private Label LblExcludeFiles;
+        private Label LblZipedType;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private ToolTip toolTip1;
+        private GroupBox groupBox3;
+        private Label LblVistaPreviaNamePackage;
+        private ToolTip toolTip2;
+        private ComboBox CmbboxProjectName;
+        private GroupBox groupBox4;
+        private GroupBox groupBox5;
+        private Label LblDestinyFolder;
+        private Button BtnDestinyFolder;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,381 @@
+﻿using Business;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.IO.Compression;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static Business.Form1Service;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace AutoZipDeploy
+{
+    public partial class Form1 : Form
+    {
+        private IConfiguration Configuration { get; set; }
+
+        private System.Windows.Forms.Timer debounceTimerBackup;
+        private System.Windows.Forms.Timer debounceTimerDeploy;
+
+        private string folderToZip;
+        private string zipFilePath;
+        private string folderSelectedDestiny;
+        private readonly Form1Service _form1Service;
+        private List<string> selectedDeleteFiles = new List<string>();// Crear una lista para almacenar los elementos seleccionados.
+        private List<string> selectedTypeBackup = new List<string>();// Crear una lista para almacenar los elementos seleccionados.
+
+
+        public Form1()
+        {
+            // Construir la configuración
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            Configuration = builder.Build();
+
+
+
+            // Leer una configuración específica
+            List<string> patternsListConfig = Configuration.GetSection("ListOfFilesToDelete").Get<List<string>>();
+
+            List<string> ProjectNamesList = Configuration.GetSection("ProjectNames").Get<List<string>>();
+
+            int? defaultOptionProjectName = Configuration.GetValue<int>("DefaultOptionProjectName");
+
+
+
+            InitializeComponent();
+            _form1Service = new Form1Service();
+            TxtBoxNameProject.Enabled = false;
+
+            lblCompleteFolderPath.AutoEllipsis = true;
+            BtnZipFolder.Enabled = false;
+
+            CmbboxProjectName.Enabled = false;
+
+
+            // Establece el texto del ToolTip que se mostrará
+            toolTip1.SetToolTip(lblCompleteFolderPath, "");
+
+            // Opcional: Configura otras propiedades
+            toolTip1.AutoPopDelay = 5000;    // Tiempo en milisegundos que el ToolTip permanece visible
+            toolTip1.ReshowDelay = 500;      // Tiempo en milisegundos antes de que el ToolTip se muestre de nuevo
+            toolTip1.ShowAlways = true;      // Muestra el ToolTip incluso si el formulario no está activo
+
+            // Opcional: Configura otras propiedades
+            toolTip2.AutoPopDelay = 5000;    // Tiempo en milisegundos que el ToolTip permanece visible
+            toolTip2.ReshowDelay = 500;      // Tiempo en milisegundos antes de que el ToolTip se muestre de nuevo
+            toolTip2.ShowAlways = true;      // Muestra el ToolTip incluso si el formulario no está activo
+
+            // Opciones disponibles de zipeado.
+            var typeZipped = new Dictionary<string, string>
+            {
+                {"1", "Backup"},
+                {"2", "Deploy"}
+            };
+
+            #region Completo Listas de opciones
+            foreach (string pattern in patternsListConfig)//web.config, app.config, etc.
+            {
+                ChkBLOptionsDelete.Items.Add(pattern, true);
+            }
+
+            foreach (string type in typeZipped.Values)//Backup, Deploy
+            {
+                ChkBLOptionsBackup.Items.Add(type, true);
+            }
+
+
+
+            CmbboxProjectName.Items.Add("");//opcion en blanco
+
+            foreach (string project in ProjectNamesList)//DirectMarketing, IM, Eflow, etc.
+            {
+                CmbboxProjectName.Items.Add(project);
+            }
+
+            try
+            {
+                CmbboxProjectName.SelectedIndex = defaultOptionProjectName ?? 0;//podria estar fuera de intervalo
+            }
+            catch (Exception)
+            {
+                CmbboxProjectName.SelectedIndex = 0;
+            }
+
+            #endregion
+
+
+
+
+
+            foreach (object item in ChkBLOptionsDelete.CheckedItems)
+            {
+                selectedDeleteFiles.Add(item.ToString());
+            }
+
+            foreach (object item in ChkBLOptionsBackup.CheckedItems)
+            {
+                selectedTypeBackup.Add(item.ToString());
+            }
+        }
+
+        private void BtnOpenDirectory_Click(object sender, EventArgs e)
+        {
+            // Crear una instancia del FolderBrowserDialog
+            using (FolderBrowserDialog folderDialog = new FolderBrowserDialog())
+            {
+                // Opcional: Configurar la descripción y el path inicial
+                folderDialog.Description = "Selecciona una carpeta";
+                folderDialog.RootFolder = Environment.SpecialFolder.MyComputer; // Puedes cambiar esto según tus necesidades
+
+                // Mostrar el diálogo al usuario
+                DialogResult result = folderDialog.ShowDialog();
+
+                // Verificar si el usuario hizo clic en el botón OK
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(folderDialog.SelectedPath))
+                {
+
+                    #region chequear si la carpeta parece ser una carpeta valida
+                    string pathToCheck = folderDialog.SelectedPath; // Modifica esto con la ruta de tu carpeta
+                    if (IsPublishFolder(pathToCheck))
+                    {
+                        // Hacer algo con la ruta seleccionada
+                        this.folderToZip = folderDialog.SelectedPath;
+                        BtnZipFolder.Enabled = true;
+                        TxtBoxNameProject.Enabled = true;
+                        CmbboxProjectName.Enabled = true;
+                        toolTip1.SetToolTip(lblCompleteFolderPath, this.folderToZip);
+                        lblCompleteFolderPath.Text = folderDialog.SelectedPath;
+                    }
+                    else
+                    {
+                        // Muestra el MessageBox
+                        DialogResult resultYesNo = MessageBox.Show($"La carpeta {pathToCheck} no parece ser una carpeta de publicación válida ¿Deseas continuar?", "Confirmación", MessageBoxButtons.YesNo);
+
+                        // Verifica la respuesta del usuario
+                        if (resultYesNo == DialogResult.Yes)
+                        {
+                            BtnZipFolder.Enabled = true;
+                            TxtBoxNameProject.Enabled = true;
+                            CmbboxProjectName.Enabled = true;
+                        }
+                        else if (resultYesNo == DialogResult.No)
+                        {
+                            BtnZipFolder.Enabled = false;
+                            TxtBoxNameProject.Enabled = false;
+                            CmbboxProjectName.Enabled = false;
+                        }
+                    }
+                    #endregion
+
+                }
+            }
+        }
+
+        private void BtnZipFolder_Click(object sender, EventArgs e)
+        {
+            string nameBackup = $"Backup {DateTime.Now.ToString("dd-MM-yyyy HH mm ss")}";
+            string nameDeploy = $"Deploy {DateTime.Now.ToString("dd-MM-yyyy HH mm ss")}";
+
+            if (CmbboxProjectName.Text != string.Empty && TxtBoxNameProject.Text != string.Empty)
+            {
+                nameBackup = $"Backup {CmbboxProjectName.Text} {TxtBoxNameProject.Text} {DateTime.Now.ToString("dd-MM-yyyy HH mm ss")}";
+                nameDeploy = $"Deploy {CmbboxProjectName.Text} {TxtBoxNameProject.Text} {DateTime.Now.ToString("dd-MM-yyyy HH mm ss")}";
+            }
+            else
+            {
+                if (TxtBoxNameProject.Text != string.Empty)
+                {
+                    nameBackup = $"Backup {TxtBoxNameProject.Text} {DateTime.Now.ToString("dd-MM-yyyy HH mm ss")}";
+                    nameDeploy = $"Deploy {TxtBoxNameProject.Text} {DateTime.Now.ToString("dd-MM-yyyy HH mm ss")}";
+                }
+                if (CmbboxProjectName.Text != string.Empty)
+                {
+                    nameBackup = $"Backup {CmbboxProjectName.Text} {DateTime.Now.ToString("dd-MM-yyyy HH mm ss")}";
+                    nameDeploy = $"Deploy {CmbboxProjectName.Text} {DateTime.Now.ToString("dd-MM-yyyy HH mm ss")}";
+                }
+            }
+
+
+            foreach (var item in selectedTypeBackup)
+            {
+                if (item.Contains("Backup"))
+                {
+                    #region BackupDeployFiles
+                    ZipPackage($"{nameBackup}", ".zip", typeOfZip.Backup);
+                    #endregion
+                }
+                if (item.Contains("Deploy"))
+                {
+                    #region Zip to deploy
+                    ZipPackage($"{nameDeploy}", ".zip", typeOfZip.Deploy);
+                    #endregion
+                }
+            }
+        }
+
+        private void ZipPackage(string fileName, string extension, typeOfZip type)
+        {
+
+            try
+            {
+                if (folderSelectedDestiny is not null)
+                {
+                    this.zipFilePath = Path.Combine(folderSelectedDestiny, fileName + extension);// Ruta y nombre del archivo zip resultante
+                }
+                else
+                {
+                    this.zipFilePath = Path.Combine(Path.GetDirectoryName(folderToZip), fileName + extension);// Ruta y nombre del archivo zip resultante
+                }
+
+                int cont = 0;
+                while (File.Exists(this.zipFilePath))
+                {
+                    this.zipFilePath = Path.Combine(Path.GetDirectoryName(folderToZip), fileName + $"({cont})" + extension);
+                    cont++;
+                }
+
+
+                _form1Service.ZipAndBackoupFiles(folderToZip, this.zipFilePath, selectedDeleteFiles, type);// Crear el archivo zip desde la carpeta
+
+                //this.folderSelectedDestiny = string.Empty;
+
+                MessageBox.Show($"Carpeta {type} zipeada exitosamente en: " + this.zipFilePath);
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show($"Error al zipear la carpeta {type} : " + ex.Message);
+                if (ex.Message.Contains("path1"))
+                {
+                    MessageBox.Show($"Error al zipear la carpeta {type} : " + "No se ha seleccionado ninguna carpeta");
+                }
+                else
+                {
+                    MessageBox.Show($"Error al zipear la carpeta {type} : " + ex.Message);
+                }
+            }
+        }
+
+
+        private void ChkBLOptionsBackup_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            if (debounceTimerBackup == null)
+            {
+                debounceTimerBackup = new System.Windows.Forms.Timer();
+                debounceTimerBackup.Interval = 100; // Tiempo de espera antes de procesar el evento
+                debounceTimerBackup.Tick += (s, args) =>
+                {
+                    debounceTimerBackup.Stop();
+                    // Procesar el cambio aquí
+                    selectedTypeBackup = new List<string>();// Crear una lista para almacenar los elementos seleccionados.
+                    foreach (object item in ChkBLOptionsBackup.CheckedItems)
+                    {
+                        selectedTypeBackup.Add(item.ToString());
+                    }
+                    if (selectedTypeBackup.Count == 0)
+                    {
+                        BtnZipFolder.Enabled = false;
+                    }
+                    else
+                    {
+                        if (this.folderToZip is not null)
+                        {
+                            BtnZipFolder.Enabled = true;
+                        }
+                    }
+                };
+            }
+
+
+
+            debounceTimerBackup.Stop();
+            debounceTimerBackup.Start(); // Reinicia el temporizador cada vez que se dispara el evento
+        }
+
+        private void ChkBLOptionsDelete_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            if (debounceTimerDeploy == null)
+            {
+                debounceTimerDeploy = new System.Windows.Forms.Timer();
+                debounceTimerDeploy.Interval = 100; // Tiempo de espera antes de procesar el evento
+                debounceTimerDeploy.Tick += (s, args) =>
+                {
+                    debounceTimerDeploy.Stop();
+                    // Procesar el cambio aquí
+                    selectedDeleteFiles = new List<string>();// Crear una lista para almacenar los elementos seleccionados.
+
+                    foreach (object item in ChkBLOptionsDelete.CheckedItems)
+                    {
+                        selectedDeleteFiles.Add(item.ToString());
+                    }
+                };
+            }
+
+            debounceTimerDeploy.Stop();
+            debounceTimerDeploy.Start(); // Reinicia el temporizador cada vez que se dispara el evento
+        }
+
+        private void TxtBoxNameProject_TextChanged(object sender, EventArgs e)
+        {
+            LblVistaPreviaNamePackage.Text = $"Backup {CmbboxProjectName.Text} {TxtBoxNameProject.Text} {DateTime.Now.ToString("dd-MM-yyyy HH mm ss")}";
+            toolTip2.SetToolTip(LblVistaPreviaNamePackage, LblVistaPreviaNamePackage.Text);
+        }
+
+        private void CmbboxProjectName_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            LblVistaPreviaNamePackage.Text = $"Backup {CmbboxProjectName.Text} {TxtBoxNameProject.Text} {DateTime.Now.ToString("dd-MM-yyyy HH mm ss")}";
+            toolTip2.SetToolTip(LblVistaPreviaNamePackage, LblVistaPreviaNamePackage.Text);
+
+        }
+
+
+
+        static bool IsPublishFolder(string path)
+        {
+            // Verifica la existencia de archivos .dll
+            bool hasDllFiles = Directory.GetFiles(path, "*.dll").Length > 0;
+
+            // Verifica la existencia de archivos de configuración específicos
+            bool hasConfigFiles = File.Exists(Path.Combine(path, "web.config")) || File.Exists(Path.Combine(path, "appsettings.json"));
+
+            // Agrega más verificaciones según sea necesario
+
+            return hasDllFiles && hasConfigFiles; // Modifica esta lógica según tus necesidades
+        }
+
+        private void BtnDestinyFolder_Click(object sender, EventArgs e)
+        {
+            // Crear una instancia del FolderBrowserDialog
+            using (FolderBrowserDialog folderDialog = new FolderBrowserDialog())
+            {
+                // Opcional: Configurar la descripción y el path inicial
+                folderDialog.Description = "Selecciona una carpeta";
+                folderDialog.RootFolder = Environment.SpecialFolder.MyComputer; // Puedes cambiar esto según tus necesidades
+
+                // Mostrar el diálogo al usuario
+                DialogResult result = folderDialog.ShowDialog();
+
+                // Verificar si el usuario hizo clic en el botón OK
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(folderDialog.SelectedPath))
+                {
+
+                    #region chequear si la carpeta parece ser una carpeta valida
+                    LblDestinyFolder.Text = folderDialog.SelectedPath;
+                    this.folderSelectedDestiny = folderDialog.SelectedPath;
+                    #endregion
+
+                }
+            }
+        }
+    }
+
+}
